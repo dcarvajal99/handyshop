7ee44e2a3787c94a43d02b5c89be9a715fac5e62
Carlos Velasquez <carlos.velasquez.sandoval@gmail.com>
2023-07-21 15:08:43 -0400

    no podia cargar la pagina sino hacia esto

--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import NavbarWithDropdown from '../components/Navbar';
 import CarouselMain from '../components/CarouselMain';
-import ContainerCards from '../components/cards/ContainerCards';
+import ContainerCards from '../components/Cards/ContainerCards';
 import FooterApp from '../components/Footer';
 import PaginationApp from '../components/PaginationApp';
 import Filtros from '../components/filtros/EstiloFiltro';
+
 
 const Main = () => {
     return (
