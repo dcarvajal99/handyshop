8eb742755bc18272d1329dd06b894712998a2f3a
Carlos Velasquez <carlos.velasquez.sandoval@gmail.com>
2023-07-24 19:46:23 -0400

    modificacion y uso de context

--- src/components/detailsServices/DetailsServices.jsx
+++ src/components/detailsServices/DetailsServices.jsx
@@ -1,135 +1,99 @@
-import { Card } from 'flowbite-react';
 import { useContext } from 'react';
 import Context from '../../context/ContextProvider';
 import { useParams } from 'react-router-dom';
-import React from 'react';
-
-// falta el use navigate del componente productcard para usar el id params
+import { Link } from 'react-router-dom';
 
 export default function DetailsServices() {
-
     const { id } = useParams();
-    const { servicios } = useContext(Context);
-    const { cart, setCart } = useContext(Context);
+    const { servicios, cart, setCart } = useContext(Context);
+    const servicio = servicios.find((servicio) => servicio.id === parseInt(id));
 
-    const clickAddToCart = () => {
-        setCart((elementosAnadidos) => {
-            const itemsAñadidos = elementosAnadidos.find((item) => item.id === id);
-            if (itemsAñadidos) {
-                return elementosAnadidos.map((item) => {
-                    if (item.id === id) {
-                        return { ...item, quantity: item.quantity + 1 };
-                    } else {
-                        return item;
-                    }
-                });
+
+
+    const ImagenUrl = 'https://www.oikos.com.co/constructora/images/website/Noticias_2019_/funciones-de-los-constructores.jpg';
+
+    if (!servicio) {
+        return <p>Servicio no encontrado</p>;
+    }
+
+    const anadirProducto = (servicio) => {
+        console.log(servicio)
+        setCart([...cart, servicio])
+    }
+
+    /*const clickAddToCart = (id) => {
+        console.log("ID del servicio a añadir al carrito:", id);
+        setCart((elementosAñadidos) => {
+            const itemfound = elementosAñadidos.find((item) => item.id === (id.toString()))
+            console.log(itemfound)
+            if (itemfound) {
+                return elementosAñadidos.map((item) =>
+                    item.id === id.toString() ? { ...item, quantity: item.quantity + 1 } : item
+                )
             } else {
-                return [...elementosAnadidos, { id, quantity: 1 }]
+                const servicioToAdd = servicios.find((servicio) => servicio.id === parseInt(id));
+                return [...elementosAñadidos, { servicioToAdd, quantity: 1 }]
             }
         })
     }
 
-    const removerItem = (id) => {
-        setCart((elementosAnadidos) => {
-            if (elementosAnadidos.find((item) => item.id === id)?.quantity === 1){
-                return elementosAnadidos.filter((item) => item.id !== id)
-            }else {
-                return elementosAnadidos.map((item) => {
-                    if(item.id === id) {
-                        return {...item,quantity: item.quantity -1}
-                    }else {
+    const clickRemoveToCart = (id) => {
+        setCart((elementosAñadidos) => {
+            if (elementosAñadidos.find((item) => item.id === id)?.quantity === 1) {
+                return elementosAñadidos.filter((item) => item.id !== id);
+            } else {
+                return elementosAñadidos.map((item) => {
+                    if (item.id === id) {
+                        return { ...item, quantity: item.quantity - 1 }
+                    } else {
                         return item
                     }
                 })
             }
-            
         })
-    }
-    /*const service = servicios.find((servicio) => servicio.id === (id));
-
-    if (!service) {
-        return <p>El servicio no fue encontrado.</p>;
     }*/
 
-    //para añadir al carrito en detailsServices
-    /*const { addToCart, cartItems } = useContext(Context);
- 
-    const clickAddToCart = (service) => {
-        addToCart(service);
-    };*/
-
-    /*en el context habria que hacer  
-    const [cartItems, setCartItems] = useState([]);
-
-  const addToCart = (service) => {
-    setCartItems((prevCartItems) => [...prevCartItems, service]);
-  };
-
-  const removeFromCart = (serviceId) => {
-    setCartItems((prevCartItems) =>
-      prevCartItems.filter((item) => item.id !== serviceId)
-    );
-  };*/
-
-    const ImagenUrl = 'https://www.oikos.com.co/constructora/images/website/Noticias_2019_/funciones-de-los-constructores.jpg'
     return (
-        /*<div className="flex items-center justify-center h-screen">
-            <div className="bg-gray-200 p-4">
-                Contenido del div centrado
-            </div>
-        </div>*/
-        /*{servicios.map((servicio) => (
-            <li key={servicio.id} className="mb-4">
-                <h2 className="text-xl font-semibold">{servicio.servicio}</h2>
-                <p>{servicio.descripcion}</p>
-                <p className="font-bold">Monto: ${servicio.monto}</p>
-                <p>Ubicación: {servicio.ubicacion}</p>
-            </li>
-        ))}*/
-
-        /*<Card 
-            horizontal
-            imgSrc={ImagenUrl}
-        >
-            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                <p>
-                    Noteworthy technology acquisitions 2021
-                </p>
-            </h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-                <p>
-                    Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
-                </p>
-            </p>
-        </Card>*/
-
-        <div className="flex items-center justify-center h-screen ">
-            <div className="w-400 h-400 bg-center bg-no-repeat bg-cover" >
+        <div className="flex items-center justify-center h-screen">
+            <div className="w-400 h-400 bg-center bg-no-repeat bg-cover">
                 <img src={ImagenUrl} alt="Descripción de la imagen" className="max-w-full max-h-full" />
             </div>
             <div className="m-l-2rem m-b-2rem w-50 text-start mx-8 my-2">
-                <h1 className='font-bold'>titulo{id}</h1>
+                <h1 className="font-bold">{servicio.servicio}</h1>
                 <div className="list-none pl-0 my-6 border-t border-gray-300">
-                    <p className="font-bold my-6">monto</p>
-                    <ul >
-                        <h2>$</h2>
-                        <h2 className='my-4'>ubicacion</h2>
+                    <p className="font-bold my-6">valor del servicio</p>
+                    <ul>
+                        <h2>${servicio.monto}</h2>
+                        <h2 className="my-4">{servicio.ubicacion}</h2>
                     </ul>
                 </div>
-                <div className="flex justify-between ">
-                    <button
-                        //onClick={() => clickAddToCart(service)} // Añade el servicio al carrito al hacer clic en el botón
-                        className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Favoritos ❤️</button>
-                    <button
-                        //onClick={() => clickAddToCart(service)} // Añade el servicio al carrito al hacer clic en el botón
-                        className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mx-4">Añadir 🛒</button>
+
+                <div className="flex justify-between">
+                    <Link to="/carrito">
+                        <button
+                            //onClick={() => clickAddToCart()} // Añade el servicio al carrito al hacer clic en el botón
+                            className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                        >
+                            Favoritos ❤️
+                        </button>
+                        <button
+                            onClick={() => anadirProducto(servicio)} // Añade el servicio al carrito al hacer clic en el botón
+                            className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mx-4"
+                        >
+                            Añadir 🛒
+                        </button>
+                        {/*<button
+                            onClick={() => clickRemoveToCart(servicio.id)} // Añade el servicio al carrito al hacer clic en el botón
+                            className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mx-4"
+                        >
+                            sacar 🛒
+    </button>*/}
+                    </Link>
                 </div>
-
-
             </div>
         </div>
-
-    )
+    );
 }
+
 
 
