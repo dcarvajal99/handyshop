b66fdf3c972f63705ee161f127e98b1193b0a468
Carlos Velasquez <carlos.velasquez.sandoval@gmail.com>
2023-07-24 19:43:45 -0400

    modificacion de vistas

--- src/views/Carrito.jsx
+++ src/views/Carrito.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Cart from "../components/cart/Cart";
+import CartTotal from "../components/cart/CartTotal";
 
 const Carrito = () => {
-    return(
-        <Cart />
+    return (
+        <>
+            <Cart />
+            <CartTotal />
+        </>
     )
 }
 
