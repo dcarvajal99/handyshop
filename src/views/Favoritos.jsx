56aca78a6c34025a77afe3716ad13f9917344e73
Carlos Velasquez <carlos.velasquez.sandoval@gmail.com>
2023-07-25 11:26:54 -0400

    eliminando archivos de prueba

--- src/views/Favoritos.jsx
+++ src/views/Favoritos.jsx
@@ -1,13 +1,9 @@
 import React from "react";
-import DetailsServices from "../components/detailsServices/DetailsServices";
-import Cart from "../components/cart/Cart";
-import ContratoExitoso from "../components/contratoExitoso/ContratoExitoso";
+
 const Favoritos = () => {
     return (
         <>
-       <ContratoExitoso />
-       <DetailsServices />
-       <Cart />
+    
         </>
     );
     }
