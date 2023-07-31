2ddb58087c4452ddb67c1e636a915513e0d7d7b0
Diego Carvajal <diego.carvajal.1515@gmail.com>
2023-07-31 16:16:06 -0400

    littlecards en product details

--- src/views/ServiceDetail.jsx
+++ src/views/ServiceDetail.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import DetailsServices from "../components/detailsServices/DetailsServices";
-
+import ContainerLittleCard from "../components/cards/littlecards/ContainerLittleCards";
 const ServiceDetail = () => {
     return (
         <>
         <DetailsServices/>
+        <ContainerLittleCard/>
         </>
     );
     }
