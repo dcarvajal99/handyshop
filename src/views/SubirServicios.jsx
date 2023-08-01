ec76af8cc532a9db9b22e343c6cc6ee896889594
LarissonRivero <Larissonrivero2@gmail.com>
2023-07-31 21:34:12 -0400

    Cambio a nivel Visual

--- src/views/SubirServicios.jsx
+++ src/views/SubirServicios.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const SubirServicios = () => {
   return (
-    <section className="bg-purple-400 dark:bg-gray-900">
-        <div className="py-8 px-4 mx-auto max-w-2xl lg:py-16">
-        <h2 className="mb-4 text-xl font-bold text-center text-gray-900 dark:text-white">Agregar un Nuevo Servicio:</h2>
+    <section className="rounded-lg h-auto max-w-lg mx-auto bg-gray-400 mb-1 m-2">
+        <div className="py-2 px-4 max-w-2xl lg:py-5">
+        <h2 className="mb-1 text-xl font-bold text-center text-gray-900 dark:text-white">Agregar un Nuevo Servicio:</h2>
             <form action="#">
                 <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
                     <div className="sm:col-span-2">
@@ -72,15 +72,15 @@ const SubirServicios = () => {
                             <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">
                                 Subir Foto
                             </label>
-                            <div className="mt-2 flex justify-center rounded-lg border border-gray-500 border-gray-900/25 px-6 py-10">
+                            <div className="mt-2 flex justify-center rounded-lg border border-gray-500 border-gray-900/25 px-2 py-2">
                                 <div className="text-center">
-                                    <svg className="mx-auto h-12 w-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                                    <svg className="mx-auto h-5 w-5 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
                                         <path fillRule="evenodd"
                                         d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z"
                                         clipRule="evenodd"/>
                                     </svg>
-                                    <div className="mt-4 flex text-sm leading-6 text-gray-600">
-                                        <label htmlFor="file-upload" className="relative cursor-pointer rounded-md bg-gray-500 font-semibold text-white focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500">
+                                    <div className="mt-2 flex text-sm leading-6 text-gray-900">
+                                        <label htmlFor="file-upload" className="relative cursor-pointer rounded-md bg-gray-900 font-semibold text-white focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500">
                                             <span>Subir Archivos</span>
                                             <input id="file-upload" name="file-upload" type="file" className="sr-only" />
                                         </label>
@@ -94,13 +94,13 @@ const SubirServicios = () => {
                             <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                             Description
                             </label>
-                            <textarea id="description"rows="8"
+                            <textarea id="description"rows="2"
                                 className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                                 placeholder="Your description here">
                             </textarea>
                     </div>
                 </div>
-                <button type="submit" className="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-dark bg-gray-400 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
+                <button type="submit" className="inline-flex px-2 py-2.5 mt-2 sm:mt-6 text-sm font-medium text-center text-gray-50 bg-gray-900 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
                     Agregar Servicio
                 </button>
             </form>
@@ -110,4 +110,5 @@ const SubirServicios = () => {
 };
 
 export default SubirServicios;
+
 
