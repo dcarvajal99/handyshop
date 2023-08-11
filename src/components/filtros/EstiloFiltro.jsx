ab66111231af14f517728ea53b009b93505ae0e4
Diego Carvajal <diego.carvajal.1515@gmail.com>
2023-08-11 03:28:00 -0400

    añadir una barra de busqueda

--- src/components/filtros/EstiloFiltro.jsx
+++ src/components/filtros/EstiloFiltro.jsx
@@ -1,52 +1,64 @@
 import React from 'react';
-import { BsFilter, BsArrowDown, BsArrowUp } from 'react-icons/bs';
 
-const Filtros = ({ handleFilterChange, handleSortChange, sortOrder, toggleSortOrder }) => {
+const Filtros = () => {
+    
     return (
-        <div className="flex items-center justify-between p-4">
-            <h2 className="text-lg font-bold">Filtros</h2>
-            <div className="flex items-center">
-                <label htmlFor="servicio" className="block font-medium mr-4">
-                    Filtrar por servicio:
-                </label>
-                <select
-                    id="servicio"
-                    name="servicio"
-                    onChange={handleFilterChange}
-                    className="w-32 px-3 py-2 border rounded-md bg-white focus:outline-none focus:ring focus:border-blue-300"
-                >
-                    <option value="">Todos</option>
-                    <option value="aseo">Aseo</option>
-                    <option value="construccion">Construcción</option>
-                    <option value="electrico">Eléctrico</option>
-                </select>
+        <>
+            <div className="bg-white flex justify-center items-center px-20">
+                <div className="flex items-center p-4 m-4 space-x-2 bg-white rounded-xl shadow-lg hover:shadow-xl transform hover:scale-105 transition duration-500">
+                    <div className="flex bg-white   w-72 space-x-4 rounded-lg ">
+                        <input className="bg-white outline-white px-5 py-2.5 	rounded-lg " type="text" placeholder="Article name or keyword..." />
+                    </div>
+                    <div className="flex px-5 py-2.5  rounded-lg text-gray-500 font-semibold cursor-pointer">
+                        <select id="pricingType" name="pricingType" className='flex py-3 px-4 rounded-lg text-gray-500 font-semibold cursor-pointe'>
+                            <option value="All" selected="">Categorias</option>
+                            <option value="Freemium">Freemium</option>
+                            <option value="Free">Free</option>
+                            <option value="Paid">Paid</option>
+                        </select>
+                    </div>
+                    <div className="text-white bg-blue-700  hover:bg-blue-800 focus:ring-4 
+                                    focus:outline-none focus:ring-blue-300 font-medium rounded-lg 
+                                    text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 
+                                    dark:focus:ring-blue-800">
+                        <span>Search</span>
+                    </div>
+                </div>
             </div>
-            <div className="flex items-center">
-                <label htmlFor="ordenarPor" className="block font-medium mr-4">
-                    Ordenar por:
-                </label>
-                <select
-                    id="ordenarPor"
-                    name="ordenarPor"
-                    onChange={handleSortChange}
-                    className="w-32 px-3 py-2 border rounded-md bg-white focus:outline-none focus:ring focus:border-blue-300"
-                >
-                    <option value="">Ninguno</option>
-                    <option value="precio">Precio</option>
-                    <option value="fecha">Fecha</option>
-                    <option value="letra">Letra</option>
-                </select>
-                {sortOrder && (
-                    <button
-                        onClick={toggleSortOrder}
-                        className="ml-2 p-2 rounded-md bg-white hover:bg-gray-100 focus:outline-none focus:ring focus:border-blue-300"
-                    >
-                        {sortOrder === 'asc' ? <BsArrowUp size={20} /> : <BsArrowDown size={20} />}
-                    </button>
-                )}
-            </div>
-        </div>
+        </>
     );
 };
 
 export default Filtros;
+
+
+/* <section className="flex flex-col md:flex-row gap-3">
+            
+            <input type="text" id="first_name" 
+            className="bg-gray-50 w-full border border-gray-300 
+            text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 
+            block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 
+            dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+            placeholder="Electricista..." required/>
+
+
+                <button type="submit" className="p-2.5 mr-2 mb-2 h-12 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Buscar</button>
+            
+
+            
+            <select id="pricingType" name="pricingType"
+                className="w-full h-10 border-2 border-gray-300 focus:outline-none focus:border-gray-300 text-gray-900 rounded px-2 md:px-3 py-0 md:py-1 tracking-wider">
+                <option value="All" selected="">All</option>
+                <option value="Freemium">Freemium</option>
+                <option value="Free">Free</option>
+                <option value="Paid">Paid</option>
+            </select>
+            <select id="pricingType" name="pricingType"
+                className="w-full h-10 border-2 border-gray-300 focus:outline-none focus:border-gray-300 text-gray-900 rounded px-2 md:px-3 py-0 md:py-1 tracking-wider">
+                <option value="All" selected="">All</option>
+                <option value="Freemium">Freemium</option>
+                <option value="Free">Free</option>
+                <option value="Paid">Paid</option>
+            </select>
+
+        </section> */
