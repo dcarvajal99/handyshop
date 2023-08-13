a4dea2cd01639c37148c4e059cf9b5b5232aeef8
Diego Carvajal <diego.carvajal.1515@gmail.com>
2023-08-13 04:03:58 -0400

    alinear en pantallas chicas

--- src/components/micuenta/cards/ContainerCards.jsx
+++ src/components/micuenta/cards/ContainerCards.jsx
@@ -4,7 +4,7 @@ const ContainerMyCards = () => {
     return (
         <section  className="dark:bg-gray-900 py-10 px-12">
             <div
-                className="grid grid-flow-row gap-8 text-neutral-600 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                className="grid justify-items-center grid-flow-row gap-8 text-neutral-600 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
                 <MyProductCard />
             </div>
         </section>
