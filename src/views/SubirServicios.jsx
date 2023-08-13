7558226a9f446023acdc9d69a1d4783053ebf963
Diego Carvajal <diego.carvajal.1515@gmail.com>
2023-08-13 00:36:53 -0400

    cambios visuales

--- src/views/SubirServicios.jsx
+++ src/views/SubirServicios.jsx
@@ -150,6 +150,7 @@ const SubirServicios = () => {
                                 onChange={handleSetServicio}
                                 value={servicio.categoria}
                                 required>
+                                <option value="">Seleccione una Categoria</option>
                                 <option>Hogar</option>
                                 <option>Mano de obra</option>
                                 <option>Cuidado personal</option>
